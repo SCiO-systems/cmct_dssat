752140c183b1b72851bb339280b8143aa94c05f6
Angelos Naoum <angelosnaoum@gmail.com>
2021-12-17 15:32:39 +0000

    update

--- dssat.py
+++ dssat.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 s3 = boto3.client('s3')
 
-os.chdir("/tmp/")
+os.chdir('/tmp/')
 
 
 def lambda_handler(event, context):
@@ -16,28 +16,27 @@ def lambda_handler(event, context):
     body = json.loads(event['body'])
     input_json = body       
 
-    r = requests.get(input_json["file_url"], allow_redirects=True)
-    open(input_json["file_name"], 'wb').write(r.content)    
+    r = requests.get(input_json['file_url'], allow_redirects=True)
+    open(input_json['file_name'], 'wb').write(r.content)    
     init_files = os.listdir()
-    print(init_files)
-    os.system("/var/task/DSSAT47/dscsm047 A " + input_json["file_name"] + " > command_line_output.txt")
+    os.system('/var/task/DSSAT47/dscsm047 A ' + input_json['file_name'] + ' > command_line_output.txt')
     
 
     all_files = os.listdir()
-    print("----------------------------")
-    print(all_files)
     output_files = [x for x in all_files if x not in init_files]
 
-    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    dt_string = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
-    dt_string = dt_string.replace("/","_")
-    dt_string = dt_string.replace(" ","_")
-    dt_string = dt_string.replace(":","_")
+    dt_string = dt_string.replace('/','_')
+    dt_string = dt_string.replace(' ','_')
+    dt_string = dt_string.replace(':','_')
     
-    for file in output_files:
-        target_bucket = "lambda-dssat"
 
-        object_name = input_json["file_name"].replace(".","_") + '_' + dt_string + "/" + file
+    files_list = []
+    for file in output_files:
+        target_bucket = 'lambda-dssat'
+
+        object_name = input_json['file_name'].replace('.','_') + '_' + dt_string + '/' + file
         
         # Upload file to S3
         try:
@@ -45,12 +44,19 @@ def lambda_handler(event, context):
         except ClientError as e:
             logging.error(e)
             return {
-                "statusCode": 500,
-                "body": json.dumps(e)
+                'statusCode': 500,
+                'body': json.dumps(e)
             }
+
+        files_list.append('https://lambda-dssat.s3.eu-central-1.amazonaws.com/' + object_name)
         os.remove(file)
 
-    return {
-        "statusCode": 200,
-        "body": "success"
+    my_output = {
+        'files_list': files_list
     }
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(my_output)
+    }
+
